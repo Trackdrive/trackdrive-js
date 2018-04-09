c06e469785d31aed309b68fa026889766ef0afa2
Roger <roger@trackdrive.net>
2018-04-09 16:04:00 -0400

    build

--- dist/trackdrive-optimizer.js
+++ dist/trackdrive-optimizer.js
@@ -10516,7 +10516,7 @@ if (typeof(window.Trackdrive) === 'undefined') {
                     td_js_v: Trackdrive.Optimizer.version
                 };
 
-                ajax_requests[unique_key] = $.ajax({
+                ajax_requests[unique_key] = jQuery.ajax({
                     url: endpoints.numbers,
                     data: data
                 })
