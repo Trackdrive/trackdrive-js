028894b64afff4e2c7b2ba772914d6bb015b6ef8
Roger <roger@trackdrive.net>
2018-04-10 16:17:33 -0400

    add data-link

--- dist/trackdrive-optimizer.js
+++ dist/trackdrive-optimizer.js
@@ -10400,8 +10400,6 @@ if (typeof(window.Trackdrive) === 'undefined') {
 
         };
 
-        var ajax_requests = {};
-
         function initialize() {
             if (typeof(offer_key) !== 'undefined' && offer_key.length === 32) {
                 show_trackdrive_numbers();
@@ -10414,9 +10412,7 @@ if (typeof(window.Trackdrive) === 'undefined') {
             find('number').each(function () {
                 // the .trackdrive-number DOM element
                 var $number = $(this);
-
                 $number.hide();
-
                 // Get additional optional tokens from the DOM element.
                 //
                 // For example, give the following HTML:
@@ -10426,12 +10422,6 @@ if (typeof(window.Trackdrive) === 'undefined') {
                 //      {"interest":"loans"}
                 //
                 var optional_tokens = $number.data('tokens');
-
-                // The number format that will be outputted. Either 'human' or 'plain'
-                var format = $number.data('format');
-                if (typeof(format) === 'undefined') {
-                    format = 'human';
-                }
                 // Request the number
                 var promise = request_trackdrive_number(optional_tokens);
                 // Wait for the server to respond
@@ -10439,16 +10429,40 @@ if (typeof(window.Trackdrive) === 'undefined') {
                     $number.show();
                 });
                 promise.done(function (data) {
-                    if (typeof(data) !== 'undefined' && typeof(data.number) !== 'undefined' && typeof(data.number.human_number) !== 'undefined') {
-                        // update the DOM with the number
-                        if (format === 'human') {
-                            $number.html(data.number.human_number);
-                        } else {
-                            $number.html(data.number.plain_number);
-                        }
-                    }
+                    draw_number($number, data);
                 });
+                // always show the number after 5 seconds in case something goes wrong
+                setTimeout(function () {
+                    $number.show();
+                }, 5000);
             });
+        }
+
+        function draw_number($number, data) {
+            var link = $number.data('hyperlink');
+            // The number format that will be outputted. Either 'human' or 'plain'
+            var format = $number.data('format');
+            if (typeof(format) === 'undefined') {
+                format = 'human';
+            }
+            if (typeof(data) !== 'undefined' && typeof(data.number) !== 'undefined' && typeof(data.number.human_number) !== 'undefined') {
+                var number = data.number;
+                // update the DOM with the number
+                var html = '';
+                if (format === 'human') {
+                    html = number.human_number;
+                } else {
+                    html = number.plain_number;
+                }
+                if (link) {
+                    if ($number.is('a')) {
+                        $number.attr('href', 'tel:' + number.plain_number.toString());
+                    } else {
+                        html = '<a href="tel:' + number.plain_number + '">' + html + '</a>';
+                    }
+                }
+                $number.html(html);
+            }
         }
 
         function request_trackdrive_number(optional_tokens) {
@@ -10461,7 +10475,7 @@ if (typeof(window.Trackdrive) === 'undefined') {
 
             var unique_key = offer_key + referrer_url + referrer_tokens;
 
-            if (typeof(ajax_requests[unique_key]) === 'undefined') {
+            if (typeof(Optimizer.ajax_requests[unique_key]) === 'undefined') {
                 // add POST data
                 var data = {
                     offer_key: offer_key,
@@ -10470,13 +10484,13 @@ if (typeof(window.Trackdrive) === 'undefined') {
                     td_js_v: Trackdrive.Optimizer.version
                 };
 
-                ajax_requests[unique_key] = TrackdrivejQuery.ajax({
+                Optimizer.ajax_requests[unique_key] = TrackdrivejQuery.ajax({
                     url: endpoints.numbers,
                     data: data
                 })
             }
 
-            return ajax_requests[unique_key];
+            return Optimizer.ajax_requests[unique_key];
         }
 
         function find(key) {
@@ -10485,6 +10499,8 @@ if (typeof(window.Trackdrive) === 'undefined') {
 
         initialize();
     };
+    // global ajax requests
+    Optimizer.ajax_requests = {};
     Optimizer.replace_numbers = function (offer_key, options) {
         new Trackdrive.Optimizer(offer_key, options);
     };
--- src/trackdrive-optimizer.js
+++ src/trackdrive-optimizer.js
@@ -30,8 +30,6 @@
 
         };
 
-        var ajax_requests = {};
-
         function initialize() {
             if (typeof(offer_key) !== 'undefined' && offer_key.length === 32) {
                 show_trackdrive_numbers();
@@ -44,9 +42,7 @@
             find('number').each(function () {
                 // the .trackdrive-number DOM element
                 var $number = $(this);
-
                 $number.hide();
-
                 // Get additional optional tokens from the DOM element.
                 //
                 // For example, give the following HTML:
@@ -56,12 +52,6 @@
                 //      {"interest":"loans"}
                 //
                 var optional_tokens = $number.data('tokens');
-
-                // The number format that will be outputted. Either 'human' or 'plain'
-                var format = $number.data('format');
-                if (typeof(format) === 'undefined') {
-                    format = 'human';
-                }
                 // Request the number
                 var promise = request_trackdrive_number(optional_tokens);
                 // Wait for the server to respond
@@ -69,16 +59,40 @@
                     $number.show();
                 });
                 promise.done(function (data) {
-                    if (typeof(data) !== 'undefined' && typeof(data.number) !== 'undefined' && typeof(data.number.human_number) !== 'undefined') {
-                        // update the DOM with the number
-                        if (format === 'human') {
-                            $number.html(data.number.human_number);
-                        } else {
-                            $number.html(data.number.plain_number);
-                        }
-                    }
+                    draw_number($number, data);
                 });
+                // always show the number after 5 seconds in case something goes wrong
+                setTimeout(function () {
+                    $number.show();
+                }, 5000);
             });
+        }
+
+        function draw_number($number, data) {
+            var link = $number.data('hyperlink');
+            // The number format that will be outputted. Either 'human' or 'plain'
+            var format = $number.data('format');
+            if (typeof(format) === 'undefined') {
+                format = 'human';
+            }
+            if (typeof(data) !== 'undefined' && typeof(data.number) !== 'undefined' && typeof(data.number.human_number) !== 'undefined') {
+                var number = data.number;
+                // update the DOM with the number
+                var html = '';
+                if (format === 'human') {
+                    html = number.human_number;
+                } else {
+                    html = number.plain_number;
+                }
+                if (link) {
+                    if ($number.is('a')) {
+                        $number.attr('href', 'tel:' + number.plain_number.toString());
+                    } else {
+                        html = '<a href="tel:' + number.plain_number + '">' + html + '</a>';
+                    }
+                }
+                $number.html(html);
+            }
         }
 
         function request_trackdrive_number(optional_tokens) {
@@ -91,7 +105,7 @@
 
             var unique_key = offer_key + referrer_url + referrer_tokens;
 
-            if (typeof(ajax_requests[unique_key]) === 'undefined') {
+            if (typeof(Optimizer.ajax_requests[unique_key]) === 'undefined') {
                 // add POST data
                 var data = {
                     offer_key: offer_key,
@@ -100,13 +114,13 @@
                     td_js_v: Trackdrive.Optimizer.version
                 };
 
-                ajax_requests[unique_key] = TrackdrivejQuery.ajax({
+                Optimizer.ajax_requests[unique_key] = TrackdrivejQuery.ajax({
                     url: endpoints.numbers,
                     data: data
                 })
             }
 
-            return ajax_requests[unique_key];
+            return Optimizer.ajax_requests[unique_key];
         }
 
         function find(key) {
@@ -115,6 +129,8 @@
 
         initialize();
     };
+    // global ajax requests
+    Optimizer.ajax_requests = {};
     Optimizer.replace_numbers = function (offer_key, options) {
         new Trackdrive.Optimizer(offer_key, options);
     };
